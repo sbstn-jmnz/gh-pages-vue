07cb38ad80f53880c8a8e84f9275739110ca7bb7
sbstn-jmnz <sbstn.jmnz@yahoo.com>
2020-11-16 19:27:43 -0300

    Se agregó formulario para crear personas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!'
+    message: 'Hello Vue!',
+    person: '',
+    people: []   
+  },
+  methods:{
+    addPerson(){
+      this.people.push(this.person)
+      this.person = ''
+    }
   }
 })
